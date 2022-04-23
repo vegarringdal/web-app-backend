deae6aabfe3b7516daec5923d654786b3276514c
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-23 16:37:17 +0200

    chore: refactor app to rad

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# web-app-server
+# web-rad-server
 
 s
--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -1,4 +1,4 @@
-describe("app-backend", () => {
+describe("rad-backend", () => {
     it("test1", (done) => {
         expect(1).toEqual(1);
         done();
