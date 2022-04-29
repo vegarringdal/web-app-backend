f07df25f110b2c377aa6a5c47022d26a135f6873
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-04-29 23:02:37 +0200

    chore: remove unused vars

--- src/utils/protectedRoute.ts
+++ src/utils/protectedRoute.ts
@@ -3,47 +3,32 @@
  * I have no idea how azure works.. need to check out msal.js
  */
 
-import { ACTIVATE_AZURE_FAKE_SUCCESS, AZURE_FAKE_ROLES, CONSOLE_INFO } from "../config";
+import { CONSOLE_INFO } from "../config";
 import { log } from "@rad-common";
 import { verifyToken } from "./verifyToken";
 
 export async function protectedRoute(req: any, res: any, next: any) {
     let error = false;
 
-    // so we can override if needed
-    if (ACTIVATE_AZURE_FAKE_SUCCESS) {
-        log(CONSOLE_INFO, "ACTIVATED FAKE USER", "ROLES GIVEN:", AZURE_FAKE_ROLES);
-
+    try {
+        const tokenVerified = await verifyToken(req.headers.authorization.split("Bearer ")[1]);
         req.user = {
-            name: "FAKE USER",
-            id: "FAKE USER ID",
-            roles: AZURE_FAKE_ROLES
+            name: tokenVerified.name,
+            id: tokenVerified.upn,
+            roles: tokenVerified.roles
         };
+        // todo, verify token
+        log(CONSOLE_INFO, "Auth token verified");
+    } catch (x) {
+        log(CONSOLE_INFO, "Auth token failed verification");
+        res.status(401).send({
+            success: false,
+            message: "autentication failed",
+            auth: false
+        });
+        error = true;
     }
 
-    if (!ACTIVATE_AZURE_FAKE_SUCCESS) {
-        try {
-            const tokenVerified = await verifyToken(req.headers.authorization.split("Bearer ")[1]);
-
-            req.user = {
-                name: tokenVerified.name,
-                id: tokenVerified.upn,
-                roles: tokenVerified.roles
-            };
-            // todo, verify token
-            log(CONSOLE_INFO, "Auth token verified");
-        } catch (x) {
-            log(CONSOLE_INFO, "Auth token failed verification");
-            res.status(401).send({
-                success: false,
-                message: "autentication failed",
-                auth: false
-            });
-            error = true;
-        }
-    } else {
-        log(CONSOLE_INFO, "Auth using fake user");
-    }
     if (!error) {
         next();
     }
